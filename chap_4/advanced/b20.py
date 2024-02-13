50377747e4c9cb088a8abd558b96ff8fb0f10e0d
pirotyyy <hiroki1423@docomo.ne.jp>
2024-02-13 21:18:14 +0900

    feat: b20.py

---
+++ chap_4/advanced/b20.py
@@ -0,0 +1,18 @@
+S = input()
+T = input()
+
+dp = [[None] * (len(T) + 1) for i in range(len(S) + 1)]
+
+dp[0][0] = 0
+for i in range(len(S) + 1):
+    for j in range(len(T) + 1):
+        if i >= 1 and j >= 1 and S[i - 1] == T[j - 1]:
+            dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1])
+        elif i >= 1 and j >= 1:
+            dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1)
+        elif i >= 1:
+            dp[i][j] = dp[i - 1][j] + 1
+        elif j >= 1:
+            dp[i][j] = dp[i][j - 1] + 1
+
+print(dp[len(S)][len(T)])
